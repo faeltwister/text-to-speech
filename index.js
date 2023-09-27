d2d98865c439e561141f88eff5a8c4df9557c6f0
faeltwister <faeltwister@gmail.com>
2023-09-27 15:06:12 -0300

    terminal

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ if(!fs.existsSync("texts")){
 if(!fs.existsSync("audios")){
     fs.mkdirSync("audios");
 }
+
+console.log(chalk.yellow("Você deseja inserir o texto pelo terminal ou por arquivo? "));
+console.log(chalk.blueBright("[1] - Terminal"));
+console.log(chalk.yellowBright("[2] - Arquivo"));
+
+prompt.start();
+prompt.get(['option'],(err,result)=>{
+    if(result.option === '1'){
+        console.log(chalk.bgGrey("Insira o texto: "));
+        prompt.get(["text"],(err,result)=>{
+
+            //salvar o texto em uma pasta
+            const text = result.text ;
+            fs.writeFileSync(`texts/texts-${Date.now()}.txt`, text);
+        });
+    }
+})
